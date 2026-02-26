ca2db7f9417394dad224d937f5625eef47fadf12
HypeCloud Macbook <apple@Mac.lan>
2026-02-26 20:21:35 +0700

    bai 2

---
+++ index.js
@@ -0,0 +1,15 @@
+function printName(name) {
+    console.log("Hello", name);
+}
+
+printName("Vy");
+printName("Khang");
+printName("Quoc");
+
+function introduce(name, age, address) {
+    console.log("Xin chao", "toi ten la", name, "nam nay toi", age, "tuoi", "Toi dang sinh song o", address);
+}
+
+introduce("Tuan", 25, "Ha Noi");
+introduce("Khang", 20, "Da Lat");
+introduce("Quoc", 30, "HCM");
