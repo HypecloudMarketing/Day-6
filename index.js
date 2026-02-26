e3f4ee5bcfb6be572efb1442db614a0ff95b2cfd
HypeCloud Macbook <apple@Mac.lan>
2026-02-26 20:52:03 +0700

    Hàm return

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-function printName(name) {
-    console.log("Hello", name);
+// function printName(name) {
+//     console.log("Hello", name);
+// }
+
+// printName("Vy");
+// printName("Khang");
+// printName("Quoc");
+
+// function introduce(name, age, address) {
+//     console.log(`Xin chao, toi ten la ${name}, nam nay toi ${age} tuoi, toi dang sinh song o ${address}`);
+// }
+
+// introduce("Tuan", 25, "Ha Noi");
+// introduce("Khang", 20, "Da Lat");
+
+function calculatePytago (canhGocVuong1, canhGocVuong2) {  
+    let canhHuyen = Math.sqrt(canhGocVuong1 * canhGocVuong1 + canhGocVuong2 * canhGocVuong2);
+    return canhHuyen;
 }
 
-printName("Vy");
-printName("Khang");
-printName("Quoc");
-
-function introduce(name, age, address) {
-    console.log(`Xin chao, toi ten la ${name}, nam nay toi ${age} tuoi, toi dang sinh song o ${address}`);
-}
-
-introduce("Tuan", 25, "Ha Noi");
-introduce("Khang", 20, "Da Lat");
+let canhGocVuong1 = 3;
+let canhGocVuong2 = 4;
+let canhHuyen = calculatePytago(canhGocVuong1, canhGocVuong2);
+console.log("Canh huyen la:", canhHuyen);
 
